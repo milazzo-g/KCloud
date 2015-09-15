bed5cf2876f5ec913413e335acd985e90ac77db7
milazzog <milazzo.ga@gmail.com>
2015-09-15 14:37:23 +0200

    Aggiunti file

---
+++ User/User.cpp
@@ -0,0 +1,6 @@
+#include "User.h"
+
+
+User::User()
+{
+}
---
+++ User/User.h
@@ -0,0 +1,13 @@
+#ifndef USER_H
+#define USER_H
+
+#include "user_global.h"
+
+class USERSHARED_EXPORT User
+{
+
+	public:
+		User();
+};
+
+#endif // USER_H
