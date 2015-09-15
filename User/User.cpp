2b4442329d39a0ec3cd5a573203b748fb51cd0d0
milazzog <milazzo.ga@gmail.com>
2015-09-15 15:26:05 +0200

    Aggiunte eccezioni.

--- User/User.cpp
+++ User/User.cpp
@@ -1,6 +1,37 @@
 #include "User.h"
 
+const char *KCloud::UserException::what() const throw (){
 
-User::User()
-{
+	return "UserException: basic exception!";
 }
+
+KCloud::UserException::Type KCloud::UserException::type() const{
+
+	return UserBasicException;
+}
+
+void KCloud::UserException::raise() const{
+
+	throw *this;
+}
+
+KCloud::UserException *KCloud::UserException::clone() const{
+
+	return new UserException(*this);
+}
+/*
+class  : public KCloud::UserException{
+	virtual const char * what() const throw () {}
+	virtual KCloud::UserException::Type type() const {}
+
+};
+*/
+class EmptyMailException : public KCloud::UserException{
+		virtual const char *				what() const throw ()	{ return "UserException: Empty mail!";					}
+		virtual KCloud::UserException::Type type() const			{ return KCloud::UserException::UserEmptyMailException;	}
+};
+
+class EmptyHashException : public KCloud::UserException{
+		virtual const char *				what() const throw ()	{ return "UserException: Empty hash!";					}
+		virtual KCloud::UserException::Type type() const			{ return KCloud::UserException::UserEmptyHashException; }
+};
--- User/User.h
+++ User/User.h
@@ -2,12 +2,106 @@
 #define USER_H
 
 #include "user_global.h"
+#include <QException>
 
-class USERSHARED_EXPORT User
-{
+namespace KCloud {
 
-	public:
-		User();
-};
+	class UserData;
+	class UserManager;
+
+	/*!
+	 * \brief The UserException class
+	 */
+	class USERSHARED_EXPORT UserException : public QException{
+
+		public:
+
+			typedef enum{
+				UserBasicException,
+				UserEmptyMailException,
+				UserEmptyHashException
+			}Type;
+
+			/*!
+			 * \brief what
+			 * \return
+			 */
+			virtual const char *	what()	const throw ();
+			/*!
+			 * \brief type
+			 * \return
+			 */
+			virtual	Type			type()	const;
+			/*!
+			 * \brief raise
+			 */
+			virtual void			raise() const;
+			/*!
+			 * \brief clone
+			 * \return
+			 */
+			virtual UserException *	clone() const;
+	};
+
+	/*!
+	 * \brief The User class
+	 */
+	class USERSHARED_EXPORT User{
+
+		friend class UserManager;
+
+		public:
+
+			typedef enum{
+				Hash,
+				Password
+			}Mode;
+
+			/*!
+			 * \brief	Costruttore
+			 * \param	_email
+			 * \param	_pass
+			 * \param	_mode
+			 */
+			User(const QString _email, const QString _pass, Mode _mode = Password) throw (UserException);
+
+			/*!
+			 * \brief	getMail
+			 * \return	L'email dell'utente.
+			 */
+			QString getMail() const;
+
+			/*!
+			 * \brief	getHash
+			 * \return	L'hash della password.
+			 */
+			QString getHash() const;
+
+			/*!
+			 * \brief	getOccupiedSpace
+			 * \return	Lo spazio occupato dall'utente.
+			 */
+			qint64	getOccupiedSpace() const;
+
+			/*!
+			 * \brief	getFreeSpace
+			 * \return	Lo spazio libero dell'utente.
+			 */
+			qint64	getFreeSpace() const;
+
+			/*!
+			 * \brief	isLogged
+			 * \return	True se l'utente è collegato, false altrimenti.
+			 */
+			bool	isLogged() const;
+
+
+
+		protected:
+
+		private:
+			UserData * const data;
+	};
+}
 
 #endif // USER_H
