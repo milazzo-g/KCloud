6a607e8233e69ebc7f521ddfea5540c63d443101
milazzog <milazzo.ga@gmail.com>
2015-09-16 12:27:23 +0200

    Abbiamo quasi finito.

--- User/user.cpp
+++ User/user.cpp
@@ -1,11 +1,114 @@
 #include "user.h"
+#include <QCryptographicHash>
+#include <QRegExp>
 
-User::User(QString nome)
-{
-	pippo = nome;
+
+const qint64	KCloud::User::totalSpace = 4294967296;
+const QString	KCloud::User::emailRegEx = "\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b"
+										   "";
+const char *KCloud::Exceptions::UserException::what() const throw (){
+	return "UserException: Unknown Exception Occured!";
 }
 
-QString User::getName()
-{
-	return pippo;
+KCloud::Exceptions::UserException::Type KCloud::Exceptions::UserException::type() const{
+	return UnknownException;
+}
+
+KCloud::Exceptions::UserException *KCloud::Exceptions::UserException::clone() const{
+	return new UserException(*this);
+}
+
+void KCloud::Exceptions::UserException::raise() const{
+	throw *this;
+}
+
+class EmailPatternMismatchException : public KCloud::Exceptions::UserException{
+		virtual const char *	what()	const throw ()	{ return "UserException: The value passed is not a valid email address!";	}
+		virtual	Type			type()	const			{ return UserException::EmailPatternMismatch;								}
+};
+
+class EmptyEmailException : public KCloud::Exceptions::UserException{
+		virtual const char *	what()	const throw ()	{ return "UserException: Email address can't be empty!";					}
+		virtual	Type			type()	const			{ return UserException::EmptyEmail;											}
+};
+
+class EmptyPasswordException : public KCloud::Exceptions::UserException{
+		virtual const char *	what()	const throw ()	{ return "UserException: Password or hash can't be empty!";					}
+		virtual	Type			type()	const			{ return UserException::EmptyPassword;										}
+};
+
+class HashToLongException : public KCloud::Exceptions::UserException{
+		virtual const char *	what()	const throw ()	{ return "UserException: The length of the hash must be 32!";				}
+		virtual	Type			type()	const			{ return UserException::HashToLong;											}
+};
+
+class NegativeSpaceException : public KCloud::Exceptions::UserException{
+		virtual const char *	what()	const throw ()	{ return "UserException: Occupied space can't be negative!";				}
+		virtual	Type			type()	const			{ return UserException::NegativeSpace;										}
+};
+
+KCloud::User::User(const QString email, const QString pass, KCloud::User::Mode mode) throw (KCloud::Exceptions::UserException){
+
+}
+
+KCloud::User::User(const KCloud::User &cpy){
+
+}
+
+QString KCloud::User::getEmail() const{
+
+}
+
+QString KCloud::User::getHash() const{
+
+}
+
+qint64 KCloud::User::getFreeSpace() const{
+
+}
+
+qint64 KCloud::User::getNetworkSize() const{
+
+}
+
+qint64 KCloud::User::getOccupiedSpace() const{
+
+}
+
+bool KCloud::User::isLogged() const{
+
+}
+
+void KCloud::User::setHash(const QString pass, KCloud::User::Mode mode) throw (KCloud::Exceptions::UserException){
+
+}
+
+void KCloud::User::setEmail(const QString email) throw (KCloud::Exceptions::UserException){
+	if(email.isEmpty()){
+		throw EmptyEmailException();
+	}
+}
+
+void KCloud::User::setLogged(){
+
+}
+
+void KCloud::User::setUnlogged(){
+
+}
+
+QDataStream &KCloud::operator <<(QDataStream &out, const KCloud::User &usr){
+
+}
+
+QDataStream &KCloud::operator >>(QDataStream &inp, KCloud::User &usr){
+
+}
+
+QDataStream &KCloud::operator <<(QDataStream &out, KCloud::User::Mode &mod){
+
+}
+
+QDataStream &KCloud::operator >>(QDataStream &inp, KCloud::User::Mode &mod){
+
 }
--- User/user.h
+++ User/user.h
@@ -2,17 +2,75 @@
 #define USER_H
 
 #include "user_global.h"
+#include <QException>
 #include <QString>
+#include <QDataStream>
 
-class USERSHARED_EXPORT User
-{
+namespace KCloud{
 
-	public:
-		User(QString nome);
-		QString getName();
+	class UserManager;
 
-	private:
-		QString pippo;
-};
+	namespace Exceptions{
+		class USERSHARED_EXPORT UserException : public QException{
+			public:
+				enum Type{
+					UnknownException,
+					EmailPatternMismatch,
+					EmptyEmail,
+					EmptyPassword,
+					HashToLong,
+					NegativeSpace
+				};
+				virtual const char *	what()	const throw ();
+				virtual	Type			type()	const;
+				virtual UserException * clone() const;
+				virtual void			raise() const;
+		};
+	}
+
+	class USERSHARED_EXPORT User{
+
+		friend class	UserManager;
+		friend			QDataStream &operator <<(QDataStream &out, const User &usr);
+		friend			QDataStream &operator >>(QDataStream &inp, User &usr);
+
+		public:
+
+			enum Mode{
+				Encrypt,
+				NotEncrypt
+			};
+
+					User(const QString email, const QString pass, Mode mode = Encrypt)	throw (Exceptions::UserException);
+					User(const User &cpy);
+
+			QString getEmail()			const;
+			QString getHash()			const;
+			qint64	getFreeSpace()		const;
+			qint64	getNetworkSize()	const;
+			qint64	getOccupiedSpace()	const;
+			bool	isLogged()			const;
+
+			void	setHash(const QString pass, Mode mode = Encrypt)					throw (Exceptions::UserException);
+			void	setEmail(const QString email)										throw (Exceptions::UserException);
+			void	setLogged();
+			void	setUnlogged();
+
+		private:
+							QString m_email;
+							QString m_hash;
+							qint64	m_space;
+							bool	m_status;
+
+			static	const	qint64	totalSpace;
+			static	const	QString emailRegEx;
+	};
+
+	QDataStream &operator <<(QDataStream &out, const User &usr);
+	QDataStream &operator >>(QDataStream &inp, User &usr);
+	QDataStream &operator <<(QDataStream &out, User::Mode &mod);
+	QDataStream &operator >>(QDataStream &inp, User::Mode &mod);
+}
+
 
 #endif // USER_H
