884576476d714471a9bf472a8df51c061e76389e
milazzog <milazzo.ga@gmail.com>
2015-09-20 23:58:06 +0200

    kjndfasdzf

--- NetObject_v_1_0/clientserver.cpp
+++ NetObject_v_1_0/clientserver.cpp
@@ -1,6 +1,21 @@
 #include "clientserver.h"
+#include <QDebug>
 
-ClientServer::ClientServer(){
-
+ClientServer::ClientServer(QObject *parent) : QThread(parent), channel(new QTcpSocket(this)), res(new KCloud::Resource(this)){
 }
 
+
+MainServer::MainServer(QObject *parent) : QTcpServer(parent){
+}
+
+void MainServer::incomingConnection(qintptr sock){
+
+	WorkerServer *thread = new WorkerServer(sock, this);
+	qDebug() << "Nuova connessione accettata!";
+}
+
+WorkerServer::WorkerServer(int fd, QObject *parent) : ClientServer(parent){
+
+	channel->setSocketDescriptor(fd);
+
+}
--- NetObject_v_1_0/clientserver.h
+++ NetObject_v_1_0/clientserver.h
@@ -1,15 +1,69 @@
 #ifndef CLIENTSERVER_H
 #define CLIENTSERVER_H
 
+#include "NetObject.h"
+#include "Resource.h"
 
-class ClientServer
-{
+#include <QThread>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+class ClientServer : public QThread{
+	Q_OBJECT
+
 	public:
-		ClientServer();
+		explicit ClientServer(QObject *parent = 0);
+
 
 	signals:
 
 	public slots:
+
+
+	protected:
+
+		QTcpSocket *channel;
+		KCloud::NetObject *res;
+
 };
+
+class WorkerServer : public ClientServer{
+	Q_OBJECT
+
+	public:
+		explicit WorkerServer(int fd, QObject *parent = 0);
+
+	protected:
+		void run();
+
+
+};
+
+
+class Client : public ClientServer{
+	Q_OBJECT
+
+	public:
+		explicit Client(QObject *parent = 0);
+		QString address;
+		quint16 port;
+
+	protected:
+		void run();
+
+};
+
+class MainServer : public QTcpServer{
+	Q_OBJECT
+
+	public:
+		explicit MainServer(QObject *parent = 0);
+
+	protected:
+		void incomingConnection(qintptr sock);
+
+
+};
+
 
 #endif // CLIENTSERVER_H
