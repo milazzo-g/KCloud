6270960831e5e203c52bddec71c1e117fad06669
milazzog <milazzo.ga@gmail.com>
2015-09-20 12:05:43 +0200

    Rimosse risorse vecchie ed utenti

---
+++ NetObject_v_1_0/Resource.cpp
@@ -0,0 +1,7 @@
+#include "Resource.h"
+
+Resource::Resource()
+{
+
+}
+
---
+++ NetObject_v_1_0/Resource.h
@@ -0,0 +1,15 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+
+class Resource : public NetObject
+{
+	public:
+		Resource();
+
+	signals:
+
+	public slots:
+};
+
+#endif // RESOURCE_H
