c8c674f6ce23eaaf68512b53948ac01780482bb0
milazzog <milazzo.ga@gmail.com>
2015-09-22 15:03:44 +0200

    inclusi progetti

--- clientServer/ClientServer.h
+++ clientServer/ClientServer.h
@@ -27,10 +27,45 @@ namespace KCloud{
 			void		execCommand(const QString &cmd);
 
 		private:
-			QCoreApplication	*m_coreApplication;
-			Console				*m_console;
+			QCoreApplication *	m_coreApplication;
+			Console	*			m_console;
 
 	};
+
+	class Engine : public QThread{
+		Q_OBJECT
+		public:
+			explicit				Engine(QObject *parent = 0);
+
+		protected:
+			QTcpSocket *			m_socket;
+
+
+	};
+
+	class WorkerServer : public Engine{
+		Q_OBJECT
+		public:
+									WorkerServer(int sd, QObject *parent = 0);
+									~WorkerServer();
+
+	};
+
+	class Client : public Engine{
+		Q_OBJECT
+		public:
+			enum WorkMode{
+				Thread,
+				Standalone
+			};
+									Client(WorkMode mode, QObject *parent = 0);
+		private:
+			QCoreApplication *		m_coreApplication;
+			Console	*				m_console;
+			WorkMode				m_workMode;
+
+	};
+
 }
 
 #endif // CLIENTSERVER_H
--- clientServer/clientServer.pro
+++ clientServer/clientServer.pro
@@ -11,9 +11,25 @@ TEMPLATE = app
 
 SOURCES += main.cpp \
     ClientServer.cpp \
-    Console.cpp
+    Console.cpp \
+    ../NetObject_v_1_0/clientserver.cpp \
+    ../NetObject_v_1_0/Commandpacket.cpp \
+    ../NetObject_v_1_0/NetObject.cpp \
+    ../NetObject_v_1_0/Resource.cpp \
+    ../NetObject_v_1_0/ResourceHeader.cpp \
+    ../NetObject_v_1_0/User.cpp \
+    ../Exceptions/Exceptions.cpp \
+    ../DatabaseManager/DatabaseManager.cpp
 
 HEADERS += \
     ClientServer.h \
-    Console.h
+    Console.h \
+    ../NetObject_v_1_0/clientserver.h \
+    ../NetObject_v_1_0/Commandpacket.h \
+    ../NetObject_v_1_0/NetObject.h \
+    ../NetObject_v_1_0/Resource.h \
+    ../NetObject_v_1_0/ResourceHeader.h \
+    ../NetObject_v_1_0/User.h \
+    ../Exceptions/Exceptions.h \
+    ../DatabaseManager/DatabaseManager.h
 
